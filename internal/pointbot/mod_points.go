30f473ed2180c61999c36f57af3be3e04e3f907c
devnull-twitch <devnull@spindelhirn.me>
2022-03-11 23:12:51 +0100

    feat: add secret message

--- internal/pointbot/mod_points.go
+++ internal/pointbot/mod_points.go
@@ -149,6 +149,12 @@ func PointModuleCommand(m tmi.Module, ffc FeatureFlagChecker) tmi.Command {
 }
 
 func (pm *pointModule) getPoints(channel, username string) *tmi.OutgoingMessage {
+	if username == "iamdevnull" {
+		return &tmi.OutgoingMessage{
+			Message: "That is a secret...",
+		}
+	}
+
 	replychan := make(chan StorageResponse)
 
 	select {
